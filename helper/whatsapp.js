c5e6121cd79fb0c42ec1194b15d1618061ffe923
adgamerx <amank.u.m100@gmail.com>
2024-05-26 13:04:01 +0530

    feat: Refactor WhatsApp bot to use environment variables for configuration

--- helper/whatsapp.js
+++ helper/whatsapp.js
@@ -5,24 +5,25 @@ const {
   useMultiFileAuthState,
   Browsers,
 } = require("@whiskeysockets/baileys");
-
+const pino = require("pino");
 require("dotenv").config(); // Load environment variables from .env file
 
 const admin = process.env.ADMIN; // The admin's phone number
-
 var groupIDs = process.env.GROUPS.split(" "); // The group IDs
 
-async function connectToWhatsApp() {
+let sock;
+
+const connectToWhatsApp = async () => {
   // load the auth info from file
   const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
 
   // create a new socket
-  const sock = makeWASocket({
+  sock = makeWASocket({
     printQRInTerminal: true,
     qrTimeout: 0, // 0 means it will never time out
     auth: state,
     version: [2, 2413, 1],
-    logconsole: false,
+    logger: pino({ level: "silent" }),
     browser: Browsers.windows("desktop"),
   });
 
@@ -52,21 +53,18 @@ async function connectToWhatsApp() {
 
   // listen for messages
   sock.ev.on("messages.upsert", async ({ messages }) => {
-    if (messages[0].key.fromMe) return; // ignore messages from self
-    // if (messages[0].key.remoteJid != admin) return; // ignore messages from other numbers
-
     console.log(messages);
+    if (messages[0].key.fromMe) return; // ignore messages from self
+    if (messages[0].key.remoteJid != admin) return; // ignore messages from other numbers
+
 
     // Test if the bot is working
-    if (messages[0].message.conversation === "test") {
+    if (messages[0].message.conversation == "test") {
       await sock.sendMessage(messages[0].key.remoteJid, {
         text: "Bot is working!🎉",
       });
       return;
-    }
-
-    // Broadcast messages to the groups
-    if (messages[0].message.conversation) {
+    } else if (messages[0].message.conversation) {
       groupIDs.forEach(async (groupID) => {
         await sock.sendMessage(groupID, {
           text: messages[0].message.conversation,
@@ -77,11 +75,36 @@ async function connectToWhatsApp() {
       });
     }
   });
-}
+
+  return sock;
+};
 
 // Function to introduce delay
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+const sendMessageToWhatsApp = async (message) => {
+  // Broadcast messages to the groups
+  if (message.text) {
+    groupIDs.forEach(async (groupID) => {
+      await sock.sendMessage(groupID, {
+        text: message.text,
+      });
+      await delay(2000);
+    });
+  } else if (message.photo) {
+    groupIDs.forEach(async (groupID) => {
+      await sock.sendMessage(groupID, {
+        text: message.caption,
+        attachment: message.photo,
+      });
+      await delay(2000);
+    });
+  };
+};
+
 // run in main file
-connectToWhatsApp();
+sock = connectToWhatsApp();
+
+module.exports = { sock, sendMessageToWhatsApp };
